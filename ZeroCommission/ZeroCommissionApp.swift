991457fed1b8cefac746e238cd5bd3a3b74dc22c
Minnnning <6alswjd6@gmail.com>
2024-10-02 20:33:33 +0900

    first commit

---
+++ ZeroCommission/ZeroCommissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroCommissionApp.swift
+//  ZeroCommission
+//
+//  Created by 김민정 on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ZeroCommissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZeroCommissionTests/ZeroCommissionTests.swift
@@ -0,0 +1,16 @@
+//
+//  ZeroCommissionTests.swift
+//  ZeroCommissionTests
+//
+//  Created by 김민정 on 10/2/24.
+//
+
+import Testing
+
+struct ZeroCommissionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
